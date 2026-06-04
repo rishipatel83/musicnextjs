6bad4251500e1f6484771a6025678b95f1fe3031
Rishi <rishi.777.patel@gmail.com>
2026-06-04 22:20:12 +0530

    Implement HeroSection component and update Home page layout

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,7 +3,6 @@ import React, { useState } from "react";
 import { HoveredLink, Menu, MenuItem, ProductItem } from "./ui/navbar-menu";
 import { cn } from "@/lib/utils";
 import Link from "next/link";
-import { div } from "motion/react-client";
 
 function Navbar({ className }: { className?: string }) {
     const [active, setActive] = useState<string | null>(null);
