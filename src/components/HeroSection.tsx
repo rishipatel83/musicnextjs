f4e10d8c32faedeab1e7d20c210dca0032ff3471
Rishi <rishi.777.patel@gmail.com>
2026-06-06 23:19:01 +0530

    Add FeaturedCourses component and integrate it into the Home page

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -6,7 +6,7 @@ import { Button } from "./ui/moving-border";
 
 function HeroSection() {
 return (
-    <div className='h-auto md:h-[40rem] w-full rounded-md flex flex-col items-center justify-center relative overflow-hidden mx-auto py-10 md:py-0'>
+    <div className='h-auto md:h-160 w-full rounded-md flex flex-col items-center justify-center relative overflow-hidden mx-auto py-10 md:py-0'>
         <Spotlight
         className="-top-40 left-0 md:-top-20 md:left-60"
         fill="white"
@@ -22,7 +22,7 @@ return (
                 <Link href={"/courses"}>
                 <Button
                 borderRadius='1.75rem'
-                className='"bg-white dark:bg-black text-black dark:text-white border-neutral-200 dark:border-slate-800'
+                className='"bg-white dark:bg-black text-black dark:text-white border-neutral-200 dark:border-slate-800 cursor-pointer'
                 >
                     Explore Courses
                 </Button>
